8da09c21b2f73c434b5c46ac56edfd9a2c154555
Magesh_M_R <mageshmr42@gmail.com>
2025-05-29 10:49:34 +0530

    initail commit for weather app

---
+++ main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+    weather_data = {}
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        if city:
+            api_key = '3a9e1dc6841d1ff705441642a720cd16'  # Replace with your OpenWeatherMap API key
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+               # print(f"[DEBUG] API Response Code: {response.status_code}")
+
+                weather_data = {
+                    'city': data['name'],
+                    'country': data['sys']['country'],
+                    'temperature': data['main']['temp'],
+                    'description': data['weather'][0]['description'],
+                    'icon': data['weather'][0]['icon'],
+                }
+                print(f"[DEBUG] Parsed Weather Data: {weather_data}")
+
+            else:
+                weather_data['error'] = 'City not found or API error.'
+                print(weather)
+
+    return render(request, 'main/index.html', {'weather': weather_data})
